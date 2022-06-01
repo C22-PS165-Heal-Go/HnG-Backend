e939fac3aecc7f85f27e529b235cbb4c5308663e
Hubertus Hans <hubertushans18@gmail.com>
2022-06-01 20:28:35 +0700

    add seeder initial controller

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,4 +1,5 @@
 import ApiController from '../controllers/api.controller';
+import SeederController from '../controllers/seeder.controller';
 import { RouterMap } from '../types/express';
 
 // General API Routes /v1
@@ -8,6 +9,13 @@ const apiRoutes: RouterMap = [
         endpoint: '/',
         handlers: [
             ApiController.helloWorldHandler
+        ]
+    },
+    {
+        method: 'get',
+        endpoint: '/seed',
+        handlers: [
+            SeederController.destinationSeeder
         ]
     },
 ];
